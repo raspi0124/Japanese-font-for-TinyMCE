6109ddb715953c69a867723a2e7a3bd036be5bf6
raspi0124 <adores0124@yahoo.co.jp>
2017-05-22 14:10:48 +0200

    Updated to beta-7

--- japanese-tinymce.php
+++ japanese-tinymce.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Japanese font for TinyMCE
 Description: Add Japanese font to TinyMCE Advanced plugin's font family selections..
-Version: beta-5
+Version: beta-7
 Author: raspi0124
 License: GPL2
 */
@@ -15,7 +15,7 @@ function tinyjpfont_load_custom_fonts($init) {
         $init['content_css'] = $init['content_css'].','.$stylesheet_url;
     }
     $font_formats = isset($init['font_formats']) ? $init['font_formats'] : 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats';
-    $custom_fonts = ';'.'ふい字=Huifont;Noto Sans Japanese=Noto Sans Japanese;太字なNoto Sans Japanese=Noto Sans Japanese 700;細字なNoto sans Japanese=Noto Sans Japanese 100';
+    $custom_fonts = ';'.'ふい字=Huifont;Noto Sans Japanese=Noto Sans Japanese;太字なNoto Sans Japanese=Noto Sans Japanese-400;細字なNoto Sans Japanese=Noto Sans Japanese-100';
     $init['font_formats'] = $font_formats . $custom_fonts;
     return $init;
 }
@@ -30,3 +30,4 @@ function tinyjpfont_style() {
 add_action( 'wp_enqueue_scripts', 'tinyjpfont_style' );
 add_action( 'admin_enqueue_scripts', 'tinyjpfont_style' );
 ?>
+
