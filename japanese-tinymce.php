2106b23109fdff3fd357cd0c7360729efe83fb44
raspi0124 <adores0124@yahoo.co.jp>
2017-09-12 09:13:31 +0200

    Update japanese-tinymce.php

--- japanese-tinymce.php
+++ japanese-tinymce.php
@@ -24,7 +24,7 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 // define $
-$config1 = 1;
+$config1 = 0;
 
 //add font to tiny mce
 function tinyjpfont_load_custom_fonts($init) {
@@ -41,20 +41,15 @@ function tinyjpfont_load_custom_fonts($init) {
 }
 add_filter('tiny_mce_before_init', 'tinyjpfont_load_custom_fonts');
 
-
 // setting <Version alpha>
-
-if( $config1 == 0 ){
-
+if( $config1 == 1 ){
+    add_action( 'wp_enqueue_scripts', 'tinyjpfont_cdncss' );
 // enque CSS at CDN
-function tinyjpfont_style() {
-    wp_register_style( 'tinyjpfont-styles', 'https://cdn.rawgit.com/raspi0124/Japanese-font-for-TinyMCE/stable/addfont.css' );
-    wp_enqueue_style( 'tinyjpfont-styles' );
+function tinyjpfont_cdncss() {
+    wp_register_style( 'tinyjpfont-cdncss', 'https://cdn.rawgit.com/raspi0124/Japanese-font-for-TinyMCE/stable/addfont.css' );
+    wp_enqueue_style( 'tinyjpfont-cdncss' );
 }
-add_action( 'wp_enqueue_scripts', 'tinyjpfont_style' );
-add_action( 'admin_enqueue_scripts', 'tinyjpfont_style' );
 }
-
 else{
 
 function tinyjpfont_style() {
