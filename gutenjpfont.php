3910924916ced4446c41242ca8dad994a40f179e
raspi0124 <adores0124@yahoo.co.jp>
2018-12-13 20:42:37 +0900

    Update to 4.00

---
+++ gutenjpfont.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * I used create-guten-block (https://github.com/ahmadawais/create-guten-block Under MIT) to minimize the developing time.
+ * Thanks to ahmadawais, the author of the tool.
+ * Official site of create-guten-block → https://ahmadawais.com/create-guten-block-toolkit/
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
