be8b98426d713543b6404a17ce30f7bbd909bbb8
raspi0124 <adores0124@yahoo.co.jp>
2019-11-10 19:19:22 +0900

    Fixed syntax

--- default-font-css.php
+++ default-font-css.php
@@ -3,12 +3,12 @@
 	$fontname = $argv[1];
 	if(isset($_GET['fn']) && $_GET['fn'] != ""){
     $fontname = $_GET['fn'];
-        if $fontname == "Noto" or $fontname == "noto"{
-            $fontname = Noto Sans Japanese;
+        if($fontname == "Noto" or $fontname == "noto"){
+					$fontname = "Noto Sans Japanese";
         }
-	}else{
-		$fontname = "Noto Sans Japanese";
-	}
+				}else{
+					$fontname = "Noto Sans Japanese";
+				}
 ?>
 body#tinymce.wp-editor {
     font-family: <?php echo $fontname; ?> !important;
